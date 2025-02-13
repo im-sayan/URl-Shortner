03ebb35ed7e83a3a4e7113c4c487ca208d37ddd5
sayan <heysayan192@gmail.com>
2025-02-13 23:02:46 +0530

    first push

--- config/redisConfig.js
+++ config/redisConfig.js
@@ -1,22 +1,22 @@
-const redis = require('redis');
+require('dotenv').config();
+const Redis = require("ioredis");
 
-const redisClient = redis.createClient({
-    host: '127.0.0.1',
-    port: 6379
-});
+// Use environment variable or fallback
+const redisClient = new Redis(process.env.REDIS_URL || "redis://127.0.0.1:6379");
 
-redisClient.on('error', (err) => {
+redisClient.on("error", (err) => {
     console.error("❌ Redis Error:", err);
 });
 
-redisClient.on('connect', () => {
+redisClient.on("connect", () => {
     console.log("✅ Redis Client Connected Successfully!");
+    console.log(`🔗 Redis Run on.. http://${process.env.REDIS_URL}`);
 });
 
-redisClient.on('ready', () => {
-    console.log("Redis is Ready to Use..🚀");
+redisClient.on("ready", () => {
+    console.log("🚀 Redis is Ready to Use!");
 });
 
-redisClient.connect();
+// No need for explicit `redisClient.connect();` in ioredis
 
 module.exports = redisClient;
