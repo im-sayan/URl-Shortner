08dc7ab2e061b7654f76363eaa20a679e61054c5
sayan <heysayan192@gmail.com>
2025-02-15 00:34:09 +0530

    first push

--- config/redisConfig.js
+++ config/redisConfig.js
@@ -1,39 +1,35 @@
-const {createClient} = require('redis')
 
-async function connectRedis() {
-    const client = createClient({
-        username: 'default',
-        password: '9HQlItz6bvn32uL957lI0kcD03HOz0bw',
-        socket: {
-            host: 'redis-17005.c232.us-east-1-2.ec2.redns.redis-cloud.com', // Ensure correct host
-            port: 17005
-        }
-    });
 
-    // const client = createClient({
+
+const { createClient } = require('redis');
+
+const redisClient = createClient({
+    username: 'default',
+    password: '9HQlItz6bvn32uL957lI0kcD03HOz0bw',
+    socket: {
+        host: 'redis-17005.c232.us-east-1-2.ec2.redns.redis-cloud.com',
+        port: 17005
+    }
+});
+
+// const redisClient = createClient({
     //     socket: {
     //         host:"127.0.0.1",
     //         port:"6379"
             
     //     }
     // });
-    
 
-    client.on('error', err => console.log('Redis Client Error', err));
+redisClient.on('error', err => console.log('Redis Client Error', err));
 
+(async () => {
     try {
-        await client.connect();
+        await redisClient.connect();
         console.log('✅ Redis connected successfully');
-    
-        await client.set('foo', 'bar');
-        const result = await client.get('foo');
-        
-        console.log('🔹 Retrieved value from Redis:', result); // >>> bar
         console.log("🚀 Redis is Ready to Use!");
     } catch (error) {
         console.error('❌ Redis connection error:', error);
     }
-    
-}
+})();
 
-connectRedis();
+module.exports = redisClient;
